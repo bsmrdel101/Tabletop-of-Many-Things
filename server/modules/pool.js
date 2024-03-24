64144609aa051bb3e06b8f8400f99344dd511a19
Bennett Smrdel <fireknights318@gmail.com>
2024-03-24 14:57:43 -0500

    fix: Add DockerFile and db config

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,39 +1,32 @@
 const pg = require('pg');
-const url = require('url');
 
 let config = {};
 
-if (process.env.DATABASE_URL) {
-  const params = url.parse(process.env.DATABASE_URL);
-  const auth = params.auth.split(':');
-
+if (process.env.NODE_ENV === 'production') {
   config = {
-    user: auth[0],
-    password: auth[1],
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1],
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
     ssl: { rejectUnauthorized: false },
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    max: 10,
+    idleTimeoutMillis: 30000,
   };
 } else {
   config = {
-    host: 'localhost', // Server hosting the postgres database
-    port: 5438, // env var: PGPORT
+    host: 'localhost',
+    port: 5438,
     database: 'app',
-    max: 10, // max number of clients in the pool
+    max: 10,
     password: 'db',
     user: 'app',
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    idleTimeoutMillis: 30000,
   };
 }
 
-// this creates the pool that will be shared by all other modules
 const pool = new pg.Pool(config);
 
-// the pool with emit an error on behalf of any idle clients
-// it contains if a backend error or network partition happens
 pool.on('error', (err) => {
   console.log('Unexpected error on idle client', err);
   process.exit(-1);
