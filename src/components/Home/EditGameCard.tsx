3eeda11a27aee66e1b3e7540c84ad6b73010fbeb
Bennett Smrdel <fireknights318@gmail.com>
2025-04-26 11:54:43 -0500

    feat: Create edit game card

---
+++ src/components/Home/EditGameCard.tsx
@@ -0,0 +1,57 @@
+import { FormEvent, useState } from "react";
+import Input from "../Library/Input";
+import Select from "../Library/Select";
+import Button from "../Library/Button";
+import { editGame } from "@/controllers/dashboardController";
+
+interface Props {
+  setOpen: (value: boolean) => void
+  refreshGames: (game: Game) => void
+  game: Game
+}
+
+
+export default function NewGameCard({ setOpen, refreshGames, game }: Props) {
+  const [name, setName] = useState(game.name);
+  const [password, setPassword] = useState(game.password ?? '');
+  const [ruleset, setRuleset] = useState(game.ruleset);
+
+  const handleEditGame = async (e: FormEvent) => {
+    e.preventDefault();
+    const newGame: Game = { ...game, name, password, ruleset };
+    await editGame(newGame);
+    refreshGames(newGame);
+    setOpen(false);
+  };
+
+
+  return (
+    <form onSubmit={handleEditGame} className="game-card game-card--form-card">
+      <Button variants={['X']} onClick={() => setOpen(false)}>X</Button>
+      <Input
+        variants={['label-thin']}
+        label="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        required
+      />
+      <Input
+        variants={['label-thin']}
+        label="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Select
+        variants={['label-thin']}
+        label="Ruleset"
+        value={ruleset}
+        onChange={(e) => setRuleset(e.target.value)}
+      >
+        <option value="5e">D&D 5E</option>
+        <option value="2024" disabled>D&D 2024</option>
+        <option value="sw" disabled>Star Wars (Fantasy Flight)</option>
+      </Select>
+      <Button variants={['small', 'dark']} type="submit">Save</Button>
+    </form>
+  );
+}
