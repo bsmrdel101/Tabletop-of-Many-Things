217c02d727f2aee73511e1a2e81e38ff35c822d0
Bennett Smrdel <fireknights318@gmail.com>
2025-09-24 22:37:03 -0500

    perf: Stop waiting for hp modifications to finish

--- src/rulesets/dnd/scripts/playerManager.ts
+++ src/rulesets/dnd/scripts/playerManager.ts
@@ -4,39 +4,38 @@ import { editCharacterHealth, editCharacterMaxHp } from "@/rulesets/dnd/services
 
 // HEALTH
 
-export const healPlayer = async (character: Character_Dnd, amount: number, room: string) => {
+export const healPlayer = (character: Character_Dnd, amount: number, room: string) => {
   if (character.hp === character.maxHp - character.maxHpDmg || amount === 0) return;
   const newHp = Math.min(character.hp + amount, character.maxHp - character.maxHpDmg);
-  await editCharacterHealth(character.id, newHp, character.tempHp);
+  editCharacterHealth(character.id, newHp, character.tempHp);
   emitServerEvent('UPDATE_PLAYER', [{ ...character, hp: newHp }, room]);
 };
 
-export const dmgPlayer = async (character: Character_Dnd, amount: number, room: string) => {
+export const dmgPlayer = (character: Character_Dnd, amount: number, room: string) => {
   if (amount === 0) return;
   const actualDmg = Math.max(0, amount - character.tempHp);
   const tempHp = Math.max(0, character.tempHp - amount);
-
   const newHp = Math.max(0, character.hp - actualDmg);
-  await editCharacterHealth(character.id, newHp, tempHp);
+  editCharacterHealth(character.id, newHp, tempHp);
   emitServerEvent('UPDATE_PLAYER', [{ ...character, hp: newHp, tempHp }, room]);
 };
 
-export const addPlayerTempHp = async (character: Character_Dnd, amount: number, room: string) => {
+export const addPlayerTempHp = (character: Character_Dnd, amount: number, room: string) => {
   if (amount <= 0) return;
-  await editCharacterHealth(character.id, character.hp, amount);
+  editCharacterHealth(character.id, character.hp, amount);
   emitServerEvent('UPDATE_PLAYER', [{ ...character, tempHp: amount }, room]);
 };
 
-export const dmgPlayerMaxHp = async (character: Character_Dnd, amount: number, room: string) => {
+export const dmgPlayerMaxHp = (character: Character_Dnd, amount: number, room: string) => {
   if (amount === 0) return;
   const newMaxHpDmg = character.maxHpDmg + amount;
   const newHp = Math.min(character.hp, character.maxHp - newMaxHpDmg);
-  await editCharacterMaxHp(character.id, character.maxHpMod, character.maxHpOverride, newMaxHpDmg, character.maxHp);
-  await editCharacterHealth(character.id, newHp, character.tempHp);
+  editCharacterMaxHp(character.id, character.maxHpMod, character.maxHpOverride, newMaxHpDmg, character.maxHp);
+  editCharacterHealth(character.id, newHp, character.tempHp);
   emitServerEvent('UPDATE_PLAYER', [{ ...character, maxHpDmg: newMaxHpDmg, hp: newHp }, room]);
 };
 
-export const restorePlayerMaxHp = async (character: Character_Dnd, room: string) => {
-  await editCharacterMaxHp(character.id, character.maxHpMod, character.maxHpOverride, 0, character.maxHp);
+export const restorePlayerMaxHp = (character: Character_Dnd, room: string) => {
+  editCharacterMaxHp(character.id, character.maxHpMod, character.maxHpOverride, 0, character.maxHp);
   emitServerEvent('UPDATE_PLAYER', [{ ...character, maxHpDmg: 0 }, room]);
 };
