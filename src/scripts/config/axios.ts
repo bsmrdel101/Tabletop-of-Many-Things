9bf746c82bffaee1918c584c04110105d7a23498
Bennett Smrdel <fireknights318@gmail.com>
2026-08-29 10:23:15 -0500

    fix: Use new api url

--- src/scripts/config/axios.ts
+++ src/scripts/config/axios.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
+import { wrapper } from 'axios-cookiejar-support';
+import { CookieJar } from 'tough-cookie';
+
 
 const getUrl = () => {
   if (import.meta.env.PROD) {
-    return 'https://tabletop-of-many-things-server.up.railway.app';
+    return 'https://api.tabletop-of-many-things.com';
   } else if (import.meta.env.VITE_NODE_ENV === 'test') {
     return 'http://localhost:8001';
   } else {
@@ -10,10 +13,14 @@ const getUrl = () => {
   }
 };
 
-const api = axios.create({
-  baseURL: getUrl(),
-  withCredentials: true,
-});
+const baseURL = getUrl();
+const jar = new CookieJar();
+
+const api = wrapper(axios.create({
+  baseURL,
+  jar,
+  withCredentials: true
+}));
 
 export const setApiBaseUrl = (url: string) => {
   api.defaults.baseURL = url;
